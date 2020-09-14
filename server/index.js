8f94d16a13f27caba00d0cbdac17b3e0204b06f0
DosImpact <ypd03008@gmail.com>
2020-09-15 00:34:09 +0900

    update command

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`✔ Server Running at ${port}`);
