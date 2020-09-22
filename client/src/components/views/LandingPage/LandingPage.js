6105f701a251e36e59c3203431f15e509575e9cb
DosImpact <ypd03008@gmail.com>
2020-09-23 01:07:27 +0900

    video/range/:fileName

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,8 +8,11 @@ function LandingPage() {
         <FaCode style={{ fontSize: "4rem" }} />
         <span style={{ fontSize: "2rem" }}>Movie Player</span>
 
-        <video width="1280" height="720" controls>
-          <source src="http://localhost:5000/api/video" type="video/mp4" />
+        <video width="1280" height="720" controls autoplay>
+          <source
+            src="http://localhost:5000/api/video/range/01"
+            type="video/mp4"
+          />
           Your browser does not support the video tag.
         </video>
       </div>
--- server/routes/video.js
+++ server/routes/video.js
@@ -34,8 +34,43 @@ router.get("/", (req, res) => {
 
 router.get("/range/:fileName", (req, res) => {
   const { fileName } = req.params;
-  console.log(fileName);
-  res.status(200).json({ sucess: true });
+  const fullPath = `videos/${fileName}.mp4`;
+  const fileState = fs.statSync(fullPath);
+  const { size } = fileState;
+  const { range } = req.headers;
+  console.log(size, range);
+  if (range) {
+    // bytes= 부분을 없애고 - 단위로 문자열을 자름
+    const parts = range.replace(/bytes=/, "").split("-");
+    // 시작 부분의 문자열을 정수형으로 변환
+    const start = parseInt(parts[0]);
+    // 끝 부분의 문자열을 정수형으로 변환 (끝 부분이 없으면 총 파일 사이즈에서 - 1)
+    const end = parts[1] ? parseInt(parts[1]) : size - 1;
+    // 내보낼 부분의 길이
+    const chunk = end - start + 1;
+    // 시작 부분과 끝 부분의 스트림을 읽음
+    console.log("범위에 대한 요청 START END CHUNK", start, end, chunk);
+    const stream = fs.createReadStream(fullPath, { start, end });
+    // 응답
+    res.writeHead(206, {
+      "Content-Range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunk,
+      "Content-Type": "video/mp4",
+    });
+    // 스트림을 내보냄
+    stream.pipe(res);
+  } else {
+    console.log("범위에 대한 요청이 아님");
+    // 범위에 대한 요청이 아님
+    res.writeHead(200, {
+      "Content-Length": size,
+      "Content-Type": "video/mp4",
+    });
+    // 스트림을 만들고 응답에 실어보냄
+    fs.createReadStream(fullPath).pipe(res);
+    // res.status(200).json({ sucess: true });
+  }
 });
 
 module.exports = router;
