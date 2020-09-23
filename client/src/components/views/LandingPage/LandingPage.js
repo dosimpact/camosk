6e383e9a10f3c7f9db5ee2b4aff762d3fa0b150d
DosImpact <ypd03008@gmail.com>
2020-09-23 12:03:27 +0900

    Data format add

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -10,7 +10,7 @@ function LandingPage() {
 
         <video width="1280" height="720" controls autoplay>
           <source
-            src="http://localhost:5000/api/video/range/01"
+            src="http://localhost:5000/api/video/range/[팔도]뽀로로키쭉쭉체조"
             type="video/mp4"
           />
           Your browser does not support the video tag.
