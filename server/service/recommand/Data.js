7fd961755bb3aeb36d94df6bc13aa589dc21fbb1
DosImpact <ypd03008@gmail.com>
2020-11-11 03:04:43 +0900

    data append

---
+++ server/service/recommand/Data.js
@@ -0,0 +1,43 @@
+export const AgeRange00 = [
+    "hc9CPpfQzWQ",
+    "w9knafLQFnQ",
+    "A1goSUPoAbA",
+    "TtAsS3Sjq64",
+];
+export const AgeRange10 = [
+    "KodjPBHq3oU",
+    "6l85mk0xtKc",
+    "D6QGOMXbyS0",
+    "BSFwCwUscIc",
+];
+export const AgeRange20 = [
+    "GzsqfWxv-HY",
+    "7pn4axXWllQ",
+    "j5Jo9RSxMhU",
+];
+export const AgeRange30 = [
+    "ao5HHZg3QMk",
+    "ENLmfcLxxT8",
+    "9bY5sJWoBqc",
+    "My9ftLuRFbM",
+];
+export const AgeRange40 = [
+    "VznE9n-FEwY",
+    "iCUEEGUCBKg",
+    "jvqo_Ceg4m4",
+    "WPOXmEGsxBQ",
+];
+export const AgeRange50 = [];
+export const AgeRange60 = [];
+export const AgeRange70 = [];
+export const AgeRange80 = [];
+export const Male = [];
+export const Female = [];
+export const Eyeglasses = [];
+export const Mustache = [];
+export const Smile = [];
+export const Sunglasses = [];
+export const ANGRY = [];
+export const HAPPY = [];
+export const SAD = [];
+export const CALM = [];
