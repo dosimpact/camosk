03887f2aff4b96777cbccc15a776cb0b937bef28
DosImpact <ypd03008@gmail.com>
2020-11-11 03:04:43 +0900

    data append

---
+++ server/service/recommand/DataGetters.js
@@ -0,0 +1,54 @@
+import {
+    ANGRY, AgeRange00, AgeRange10,
+    AgeRange20, AgeRange30, AgeRange40,
+    AgeRange50, AgeRange60, AgeRange70,
+    AgeRange80, CALM, Eyeglasses, Female,
+    HAPPY, Male, Mustache, SAD, Smile, Sunglasses
+} from "./Data"
+
+// 10 
+
+const RandomeIdx = (arr) => Math.floor(Math.random() * arr.length);
+const DefaultV = "3MSPHzlRXQQ";
+
+const getRandomeAgeRange00 = () => { }
+const getRandomeAgeRange10 = () => { }
+const getRandomeAgeRange20 = () => { }
+const getRandomeAgeRange30 = () => { }
+const getRandomeAgeRange40 = () => { }
+const getRandomeAgeRange50 = () => { }
+const getRandomeAgeRange60 = () => { }
+const getRandomeAgeRange70 = () => { }
+const getRandomeAgeRange80 = () => { }
+const getRandomeMale = () => { }
+const getRandomeFemale = () => { }
+const getRandomeEyeglasses = () => { }
+const getRandomeMustache = () => { }
+const getRandomeSmile = () => { }
+const getRandomeSunglasses = () => { }
+const getRandomeANGRY = () => { }
+const getRandomeHAPPY = () => { }
+const getRandomeSAD = () => { }
+const getRandomeCALM = () => { }
+
+export {
+    getRandomeAgeRange00,
+    getRandomeAgeRange10,
+    getRandomeAgeRange20,
+    getRandomeAgeRange30,
+    getRandomeAgeRange40,
+    getRandomeAgeRange50,
+    getRandomeAgeRange60,
+    getRandomeAgeRange70,
+    getRandomeAgeRange80,
+    getRandomeMale,
+    getRandomeFemale,
+    getRandomeEyeglasses,
+    getRandomeMustache,
+    getRandomeSmile,
+    getRandomeSunglasses,
+    getRandomeANGRY,
+    getRandomeHAPPY,
+    getRandomeSAD,
+    getRandomeCALM,
+}
