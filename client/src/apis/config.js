0c8f5487664b622342b08fc50488e976663d59fa
Doyoung <doyoung@Doyoungs-MacBook-Pro.local>
2020-11-23 14:57:29 +0900

    ?

--- client/src/apis/config.js
+++ client/src/apis/config.js
@@ -1,4 +1,4 @@
-export const BASE_SERVER_URL = "http://localhost:5000/";
-// export const BASE_SERVER_URL = "http://133.186.221.101:3000/";
+// export const BASE_SERVER_URL = "http://localhost:5000/";
+export const BASE_SERVER_URL = "http://133.186.221.101:5000/";
 
 // export const BASE_URL = "http://localhost:5000/api/recommand/v2/";
