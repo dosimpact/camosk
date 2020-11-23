0f836b279c1b3f302df1b3d6f9856f7cac4a4f94
DosImpact <ypd03008@gmail.com>
2020-11-24 00:29:53 +0900

    RedBellC

--- client/src/components/camosks/RedBell/RedBellC.js
+++ client/src/components/camosks/RedBell/RedBellC.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
-import RedBellP from "./RedBellP"
+import React, { useState } from "react";
+import LocationLoad from "components/camosks/Location/LocationLoad";
+import { ADMIN_PH } from "apis/config";
+import { SMS } from "apis/notification";
 
+import RedBellP from "./RedBellP";
 
 // 현재시간을 가져와서, 14:00 , 12월 31일 등으로 표시하기
 // moment .js 사용
 const RedBellC = (props) => {
-    const [count, setCount] = useState(0);
-    const handleCount = (e) => {
-        setCount(prev => prev + 1);
-        if (count >= 2) {
-            console.log("비상벨 신고됨")
-        }
+  const [count, setCount] = useState(0);
+  const PushNotification = async () => {
+    const location = await LocationLoad();
+    console.log("location", location);
+    await SMS(`${location} 비상벨 신고 들어옴, 즉시 출동 바람`, ADMIN_PH);
+  };
+  const handleCount = (e) => {
+    setCount((prev) => prev + 1);
+    if (count >= 2) {
+      console.log("비상벨 신고됨");
+      PushNotification();
     }
-    return (
-        <>
-            <RedBellP {...props} count={count} handleCount={handleCount} />
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <RedBellP {...props} count={count} handleCount={handleCount} />
+    </>
+  );
+};
 
-export default RedBellC
+export default RedBellC;
