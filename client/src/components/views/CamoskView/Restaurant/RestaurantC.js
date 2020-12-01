3d4438c7de19d1dc26d694387c96797638227ae1
DosImpact <ypd03008@gmail.com>
2020-12-01 19:32:40 +0900

    order

--- client/src/components/views/CamoskView/Restaurant/RestaurantC.js
+++ client/src/components/views/CamoskView/Restaurant/RestaurantC.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import RestaurantP from "./RestaurantP"
+import React, { useState } from "react";
+import RestaurantP from "./RestaurantP";
+import { getRecommand } from "apis/reommand";
+
 /*
     매장 안의 키오스크를 구성한다.
 */
 const RestaurantC = () => {
-    return (
-        <>
-            <RestaurantP />
-        </>
-    )
-}
+  const [hasPerson, setHasPerson] = useState(false);
 
-export default RestaurantC
+  const [urlTop, setUrlTop] = useState(null);
+  const [urlBottom, setUrlBottom] = useState(null);
+
+  const handle_onTarget = async (target) => {
+    const { data: data1 } = await getRecommand(target);
+    const { data: data2 } = await getRecommand(target);
+    console.log(data1);
+    console.log(data2);
+    setUrlTop(data1?.url);
+    setUrlBottom(data2?.url);
+  };
+
+  return (
+    <>
+      <RestaurantP
+        handle_onTarget={handle_onTarget}
+        hasPerson={hasPerson}
+        setHasPerson={setHasPerson}
+        urlTop={urlTop}
+        urlBottom={urlBottom}
+      />
+    </>
+  );
+};
+
+export default RestaurantC;
--- client/src/components/views/CamoskView/Restaurant/RestaurantP.js
+++ client/src/components/views/CamoskView/Restaurant/RestaurantP.js
@@ -6,12 +6,22 @@ import Order from "components/camosks/Order";
 // import NewsC from "components/camosks/News/NewsC"
 // import WeatherC from "components/camosks/Weather/WeatherC"
 // import QRCodeC from "components/camosks/QRCode/QRCodeC"
-import SampleAds from "components/camosks/SampleAds/SampleAdsDummy";
+import AdvertisePanel from "components/camosks/SampleAds/SampleAdsDummy";
+import FaceRekogCam from "components/camosks/FaceFeedCam/FaceRekogCam";
+import FaceInfoCam from "components/camosks/FaceInfoCam/FaceInfoCam";
 
+// import PeopleCapture from "../../../camosks/People/PeopleCapture";
+import CriminalCapture from "../../../camosks/Criminal/CriminalCapture";
 /*
     매장 안의 키오스크를 구성한다.
 */
-const RestaurantP = () => {
+const RestaurantP = ({
+  hasPerson,
+  setHasPerson,
+  handle_onTarget,
+  urlTop,
+  urlBottom,
+}) => {
   useEffect(() => {
     window.scrollTo(0, 70);
     return () => {};
@@ -21,20 +31,35 @@ const RestaurantP = () => {
       <Container>
         <Order />
       </Container>
-
-      {/* 
-            <div className="first">
-                <ClockC />
-                <WeatherC />
-            </div>
-
-            <Container className="second">
-                <NewsC />
-            </Container> */}
-
       <Container>
-        <SampleAds />
+        {hasPerson && urlTop && urlBottom ? (
+          <>
+            {/* 사람이 있는경우 */}
+            <AdvertisePanel
+              url={
+                urlTop ? urlTop : "https://www.youtube.com/watch?v=zoGg0KPa4a0"
+              }
+            />
+          </>
+        ) : (
+          <>
+            {/* 사람이 없는 경우 */}
+            <AdvertisePanel url="https://www.youtube.com/watch?v=HxhjperItvI" />
+          </>
+        )}
       </Container>
+      <FaceRekogCam setHasPerson={setHasPerson} />
+      <FaceInfoCam
+        trigger={hasPerson}
+        onChange={(target) => {
+          console.log("FaceInfoCam target", target);
+          handle_onTarget(target);
+        }}
+      />
+      {/* The Webcam Component PeopleCapture is attached */}
+      {/*<PeopleCapture hasPerson={hasPerson}/>*/}
+      {<CriminalCapture hasPerson={hasPerson} />}
+      {/*우선 엘레베이터에 범죄자 찾기 기능 구현*/}
     </Wrapper>
   );
 };
