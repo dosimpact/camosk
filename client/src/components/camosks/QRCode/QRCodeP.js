693db554e024387ede2e2b09420f6bd04e9feaa1
DosImpact <ypd03008@gmail.com>
2020-12-01 21:59:29 +0900

    RoadP

--- client/src/components/camosks/QRCode/QRCodeP.js
+++ client/src/components/camosks/QRCode/QRCodeP.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react'
-import QRcode from 'qrcode';
+import React, { useState } from "react";
+import QRcode from "qrcode";
 
 function QRCodeP({ url, sizeable = false, className }) {
-    const [code, setCode] = useState("")
-    const [width, setWidth] = useState(200)
+  const [code, setCode] = useState("");
+  const [width, setWidth] = useState(200);
 
-    const handleHover = (e, param) => {
-        e.preventDefault();
-        if (param === 1 && sizeable) {
-            setWidth(360)
-        } else {
-            setWidth(200)
-        }
-
-    }
-    QRcode.toDataURL(url).then(codeUrl => setCode(codeUrl))
-    if (code.length === 0) {
-        return <p>QRCode is being built...</p>
+  const handleHover = (e, param) => {
+    e.preventDefault();
+    if (param === 1 && sizeable) {
+      setWidth(500);
     } else {
-        return (<div style={{ width: width, height: width }} className={className}>
-            <img
-                src={code}
-                alt="ad-qrcode"
-                style={{ width: "100%", height: "100%" }}
-                onMouseOver={(event) => handleHover(event, 1)}
-                onMouseLeave={(event) => handleHover(event, 0)}
-            />
-        </div>
-        )
+      setWidth(500);
     }
+  };
+  QRcode.toDataURL(url).then((codeUrl) => setCode(codeUrl));
+  if (code.length === 0) {
+    return <p>QRCode is being built...</p>;
+  } else {
+    return (
+      <div className={className}>
+        <img
+          src={code}
+          alt="ad-qrcode"
+          style={{ width: "100%", height: "100%" }}
+          onMouseOver={(event) => handleHover(event, 1)}
+          onMouseLeave={(event) => handleHover(event, 0)}
+        />
+      </div>
+    );
+  }
 }
 
-export default QRCodeP
+export default QRCodeP;
--- client/src/components/views/CamoskView/Road/RoadP.js
+++ client/src/components/views/CamoskView/Road/RoadP.js
@@ -1,14 +1,120 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Order from "components/camosks/Order";
 
+// import ClockC from "components/camosks/Clock/ClockC"
+// import NewsC from "components/camosks/News/NewsC"
+// import WeatherC from "components/camosks/Weather/WeatherC"
+// import QRCodeC from "components/camosks/QRCode/QRCodeC"
+import AdvertisePanel from "components/camosks/SampleAds/CustomAds";
+import FaceRekogCam from "components/camosks/FaceFeedCam/v2/FaceRekogCam";
+import FaceInfoCam from "components/camosks/FaceInfoCam/FaceInfoCam";
+import QRCode from "components/camosks/QRCode/QRCodeP";
+
+// import PeopleCapture from "../../../camosks/People/PeopleCapture";
+import CriminalCapture from "../../../camosks/Criminal/CriminalCapture";
 /*
     매장 안의 키오스크를 구성한다.
 */
-const RoadP = () => {
-    return (
-        <div>
-            RoadP
-        </div>
-    )
-}
+const RoadP = ({
+  hasPerson,
+  setHasPerson,
+  handle_onTarget,
+  urlTop,
+  urlBottom,
+}) => {
+  useEffect(() => {
+    window.scrollTo(0, 70);
+    return () => {};
+  }, []);
 
-export default RoadP
+  return (
+    <Wrapper>
+      <Container>
+        <div className="headerPart">
+          <div className="column">
+            <FaceRekogCam className=" FaceRekogCam" onChange={setHasPerson} />
+          </div>
+          <div className="column">
+            <QRCode
+              className="QRCode"
+              url="https://www.youtube.com/watch?v=HxhjperItvI"
+            />
+          </div>
+          <div className="column">
+            {hasPerson && urlBottom ? (
+              <AdvertisePanel
+                className="AdvertisePanel"
+                url={
+                  urlBottom
+                    ? urlBottom
+                    : "https://www.youtube.com/watch?v=zoGg0KPa4a0"
+                }
+              />
+            ) : (
+              <AdvertisePanel
+                className="AdvertisePanel"
+                url="https://www.youtube.com/watch?v=HxhjperItvI"
+              />
+            )}
+          </div>
+        </div>
+      </Container>
+
+      {/* invisible area */}
+      <FaceInfoCam
+        trigger={hasPerson}
+        onChange={(target) => {
+          console.log("FaceInfoCam target", target);
+          handle_onTarget(target);
+        }}
+      />
+      {<CriminalCapture hasPerson={hasPerson} />}
+    </Wrapper>
+  );
+};
+
+export default RoadP;
+
+const Wrapper = styled.div`
+  width: 100%;
+  & .headerPart {
+    display: flex;
+    flex-flow: row nowrap;
+    & .column {
+      height: 180px;
+      flex: 1;
+    }
+    & .column:nth-child(1) {
+      min-width: 250px;
+      max-width: 250px;
+      flex: 1 1auto;
+    }
+    & .FaceRekogCam {
+      height: 100%;
+      width: 100%;
+    }
+  }
+  & .first {
+    padding-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  & .second {
+    margin-top: 50px;
+  }
+
+  & .AdvertisePanel {
+    /* width: 80vw; */
+    height: 100%;
+  }
+
+  & .QRCode {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Container = styled.div``;
