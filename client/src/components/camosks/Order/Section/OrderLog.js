b2de9163c2a6fcfe2943e4d569b72536542ab751
DosImpact <ypd03008@gmail.com>
2020-12-01 21:59:29 +0900

    RoadP

---
+++ client/src/components/camosks/Order/Section/OrderLog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const OrderLog = () => {
+  return (
+    <Container>
+      <div>얼굴을 인식시켜 주세요 구매 정보가 나옵니다.</div>
+    </Container>
+  );
+};
+
+export default OrderLog;
+
+const Container = styled.div``;
