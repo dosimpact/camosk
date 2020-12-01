5b42c7177b84fd0b3b976fc5c95d747f1fdf483c
DosImpact <ypd03008@gmail.com>
2020-12-01 22:09:39 +0900

    readyto merge

--- client/src/components/camosks/Order/Section/OrderLog.js
+++ client/src/components/camosks/Order/Section/OrderLog.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const OrderLog = () => {
+const OrderLog = ({ name, onChange }) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <Container>
       <div>얼굴을 인식시켜 주세요 구매 정보가 나옵니다.</div>
